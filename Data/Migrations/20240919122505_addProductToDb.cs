7c9d6adeea9b6f9fda9b03859ecc8054588e5f79
Sergei Brunner <sbrunner@quadus.de>
2024-09-19 15:54:59 +0200

    Product created

---
+++ Data/Migrations/20240919122505_addProductToDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YumBlazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Product",
+                newName: "SpecialTag");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SpecialTag",
+                table: "Product",
+                newName: "MyProperty");
+        }
+    }
+}
